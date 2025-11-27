7cdfb8ab3d821aafd122da9820d98da6cc12b386
NQKhanh296 <156611827+NQKhanh296@users.noreply.github.com>
2025-11-28 00:41:13 +0100

    hotove AppConfig.cs s dokumentaci

--- ParalelDownloader/src/Config/AppConfig.cs
+++ ParalelDownloader/src/Config/AppConfig.cs
@@ -1,26 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ParalelDownloader.src.Config
+﻿namespace ParalelDownloader.src.Config
 {
     public class AppConfig
     {
-        public string[] UrlsToDownload { get; set; } = new[]
-        {
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/500px-Cat_November_2010-1a.jpg",
-        };
-
         /// <summary>
-        /// Vrátí počet workerů dle počtu CPU jader a počtu URL.
+        /// AppConfig vypočítává optimální počet workerů pro thread pool.
+        /// Počet workerů = minimum mezi počtem URL a počtem CPU jader.
         /// </summary>
-        public int CalculateOptimalWorkers()
+        public int CalculateOptimalWorkers(int urlCount)
         {
             int cpuCores = Environment.ProcessorCount;
-            int urlCount = UrlsToDownload.Length;
-
             return Math.Min(cpuCores, urlCount);
         }
     }
