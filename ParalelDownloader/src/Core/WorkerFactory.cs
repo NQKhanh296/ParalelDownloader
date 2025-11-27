8c1dc7adb3b5c771253b6dde7f9ad5dd1db7d159
NQKhanh296 <156611827+NQKhanh296@users.noreply.github.com>
2025-11-27 19:37:38 +0100

    Factory pattern for DownloadWorkers

--- ParalelDownloader/src/Core/WorkerFactory.cs
+++ ParalelDownloader/src/Core/WorkerFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ParalelDownloader.src.Downloader;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace ParalelDownloader.src.Core
 {
-    internal class WorkerFactory
+    public class WorkerFactory
     {
+        public DownloadWorker CreateWorker(int id, DownloadQueue queue, SemaphoreSlim semaphore)
+        {
+            return new DownloadWorker(id, queue, semaphore);
+        }
     }
 }
