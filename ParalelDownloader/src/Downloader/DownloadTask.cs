687bf4f02aec46cfb4bf3a68674b2d4d1a000c9d
NQKhanh296 <156611827+NQKhanh296@users.noreply.github.com>
2025-11-27 18:27:17 +0100

    setting up projekt structure

---
+++ ParalelDownloader/src/Downloader/DownloadTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParalelDownloader.src.Downloader
+{
+    internal class DownloadTask
+    {
+        public string Url { get; }
+        public string FileName { get; }
+
+        public DownloadTask(string url, string fileName)
+        {
+            Url = url;
+            FileName = fileName;
+        }
+    }
+}
